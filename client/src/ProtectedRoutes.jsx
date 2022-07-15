ec87404c2267f9100f5b5a351181b79930465c9a
jywang77 <105957944+jywang77@users.noreply.github.com>
2022-07-15 17:15:58 -0400

    axios

--- client/src/ProtectedRoutes.jsx
+++ client/src/ProtectedRoutes.jsx
@@ -1,20 +1,21 @@
 import { Navigate, Outlet } from "react-router";
-import { axios } from "axios";
+import axios from "axios";
 import { useState, useEffect } from "react";
 
 const useAuth = () => {
   // grabbing whether the user is authenticated from back end
   const [isAuth, setIsAuth] = useState(true);
 
-  // useEffect(() => {
-  //   axios
-  //     .get("http://localhost:3001/users/current")
-  //     .then((res) => setIsAuth(res.data))
-  //     .catch((err) => console.err(err));
-  // }, []);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/users/current")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.err(err));
+  }, []);
 
-  const user = { loggedIn: isAuth };
-  return user && user.loggedIn;
+  return { loggedIn: isAuth };
 };
 
 const ProtectedRoutes = () => {
